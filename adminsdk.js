4dcff3882dd3cdb4a9b77a8c315e667837eee8f0
Muganwa <stevenmuganwa@gmail.com>
2020-02-07 11:18:01 +0300

    method to convert strings to spaces

--- adminsdk.js
+++ adminsdk.js
@@ -4,7 +4,7 @@ module.exports = {
     "type": "service_account",
     "project_id": "kilembe-school",
     "private_key_id": process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    "private_key": (process.env.FIREBASE_ADMIN_PRIVATE_KEY).replace(/\\n/g, '\n'),
     "client_email": "firebase-adminsdk-1canj@kilembe-school.iam.gserviceaccount.com",
     "client_id": "104742052484276817005",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
